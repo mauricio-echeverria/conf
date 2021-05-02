39e508bc264152f452872d2cc052cff3173ba568
mauricio-echeverria <48423074+mauricio-echeverria@users.noreply.github.com>
2021-05-02 11:55:30 -0400

    Create conf.sh

---
+++ conf.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "[server-startup] Executing injector-secrets-manager secret.env"
+
+if [ $STATUS_SECRETS_VIEW = 'true' ];
+then
+echo 'See Secrets'
+cat /var/secrets/env/secret.env
+export $(cat /var/secrets/env/secret.env | xargs)
+#source /var/secrets/env/secret.env
+rm /var/secrets/env/secret.env
+fi
+
+if [ $STATUS_SECRETS_VIEW != 'true' ];
+then
+export $(cat /var/secrets/env/secret.env | xargs)
+#source /var/secrets/env/secret.env
+rm /var/secrets/env/secret.env
+fi
+
+echo "-----------------------------------"
+echo $FTP_HOST
+echo $FTP_USER
+echo $FTP_PASS
+echo "-----------------------------------"
+
+sleep 3600
